d41a7c272bbf4c3d6cf80bff241f735cc62420bb
Zuoyan Qin <qinzuoyan@gmail.com>
2014-05-30 13:57:53 +0800

    fix compile_proto.sh to add proto_path

--- src/compile_proto.sh
+++ src/compile_proto.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 PROTOC=$PROTOBUF_DIR/bin/protoc
+PROTO_INCLUDE=$PROTOBUF_DIR/include
 
-$PROTOC --proto_path=. --cpp_out=. sofa/pbrpc/rpc_meta.proto sofa/pbrpc/rpc_option.proto sofa/pbrpc/builtin_service.proto
+$PROTOC --proto_path=. --proto_path=$PROTO_INCLUDE --cpp_out=. \
+sofa/pbrpc/rpc_meta.proto sofa/pbrpc/rpc_option.proto sofa/pbrpc/builtin_service.proto
 
