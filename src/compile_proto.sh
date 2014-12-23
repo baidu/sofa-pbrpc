aae080917e88701a99fcda9e2b8eff2c025551a6
koalademo <tianming816@163.com>
2014-12-23 08:28:30 +0800

    Update compile_proto.sh

--- src/compile_proto.sh
+++ src/compile_proto.sh
@@ -3,6 +3,6 @@
 PROTOC=$PROTOBUF_DIR/bin/protoc
 PROTO_INCLUDE=$PROTOBUF_DIR/include
 
-$PROTOC --proto_path=. --proto_path=$PROTO_INCLUDE --cpp_out=. \
+$PROTOC --proto_path=. --proto_path=$PROTO_INCLUDE --proto_path=/usr/local/include --cpp_out=. \
 sofa/pbrpc/rpc_meta.proto sofa/pbrpc/rpc_option.proto sofa/pbrpc/builtin_service.proto
 
