bb16cb3af12d102f2fb74bcf92569e5862f2a64e
qinzuoyan <qinzuoyan@gmail.com>
2015-04-05 23:01:36 +0800

    improve client_sample.py

--- python/sample/client_sample.py
+++ python/sample/client_sample.py
@@ -10,27 +10,41 @@
 #   * The protobuf python lib has been installed.
 #   * The sofa-pbrpc python lib has been installed.
 #   * The server in ../../sample/echo has been started.
+#
+# For more, please refer to `./README'.
 
-import time
-import echo_service_pb2
 from sofa.pbrpc import client
+import echo_service_pb2
+import sys
 
+# Create service stub.
 channel = client.Channel('127.0.0.1:12321')
 service = echo_service_pb2.EchoServer_Stub(channel)
 
+# Create controller.
+# We set timeout to 1.5 seconds by controller.SetTimeout() method.
 controller = client.Controller()
-controller.SetTimeout(1)
+controller.SetTimeout(1.5)
 
+# Prepare request.
 request = echo_service_pb2.EchoRequest()
 request.message = 'Hello World'
 
-start = time.time()
-response = service.Echo(controller, request)
-end = time.time()
-print "Elasped Time: %.6f seconds" % (end - start)
+# Call method.
+try:
+  response = service.Echo(controller, request)
+except client.TimeoutError:
+  print "ERROR: RPC timeout"
+  sys.exit(1)
+except Exception as e:
+  print "ERROR: RPC fail: %s" % e
+  sys.exit(1)
 
+# Check remote failure.
 if controller.Failed():
-  print "ERROR: [%d] %s" % (controller.ErrorCode(), controller.ErrorText())
-else:
-  print "Response:\n\n%s" % response
+  print "ERROR: Remote fail: %s" % controller.ErrorText()
+  sys.exit(1)
+
+# OK, print response.
+print "Response:\n\n%s" % response
 
