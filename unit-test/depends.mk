e3066adaf1d1b6e21fd42e77c88504bb54a4da9e
qinzuoyan <qinzuoyan@gmail.com>
2015-12-03 11:45:09 +0800

    run unit-test in travis.yml

--- unit-test/depends.mk
+++ unit-test/depends.mk
@@ -13,6 +13,6 @@
 ##   $(GTEST_DIR)/src/gtest-all.cc
 ##
 #GTEST_DIR=/home/users/qinzuoyan01/libs/gtest-1.7.0
-GTEST_DIR=
+GTEST_DIR=../googletest/googletest
 ###############################################################
 
