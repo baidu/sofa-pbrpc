a4c69622b61d3be72095b1bd1f73eded949b7fb5
cyshi <wbstsa@gmail.com>
2015-12-10 22:57:00 +0800

    add header file

---
+++ src/sofa/pbrpc/http.h
@@ -0,0 +1,53 @@
+// Copyright (c) 2014 Baidu.com, Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Author: shichengyi@baidu.com (Shi Chengyi)
+
+#ifndef _SOFA_PBRPC_HTTP_H_
+#define _SOFA_PBRPC_HTTP_H_
+
+#include <map>
+#include <string>
+
+namespace sofa {
+namespace pbrpc {
+
+struct HTTPRequest
+{
+    enum Type
+    {
+        GET = 1,
+        POST = 2
+    };
+
+    Type type;
+
+    std::map<std::string, std::string> headers;
+
+    std::map<std::string, std::string> query_params;
+
+    std::string path;
+
+    std::string body;
+
+    HTTPRequest() : type(GET)
+                  , headers()
+                  , query_params()
+                  , path("") 
+                  , body("")
+    { }
+};
+
+struct HTTPResponse
+{
+    std::string html;
+
+    HTTPResponse() : html("")
+    { }
+};
+
+} // namespace pbrpc
+} // namespace sofa
+
+#endif
